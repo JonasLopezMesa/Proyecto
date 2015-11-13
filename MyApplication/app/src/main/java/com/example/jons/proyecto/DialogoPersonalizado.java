ad6a69a28cac5a3148631cd54fc7d88867e8e698
jolome777 <77jonas77@gmail.com>
2015-11-13 19:26:48 +0000

    Eliminada la carpeta esa rara y añadidas fotos del mockup

--- MyApplication/app/src/main/java/com/example/jons/proyecto/DialogoPersonalizado.java
+++ MyApplication/app/src/main/java/com/example/jons/proyecto/DialogoPersonalizado.java
@@ -1,5 +1,6 @@
 package com.example.jons.proyecto;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -18,6 +19,17 @@ public class DialogoPersonalizado extends DialogFragment {
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 	    LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+		getActivity().getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+							| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+							| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+							| View.SYSTEM_UI_FLAG_FULLSCREEN
+							| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
 		builder.setView(inflater.inflate(R.layout.dialog_personal, null));
 		/*.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
